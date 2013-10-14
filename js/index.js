5b899aaf243df065f6c4b2013ca7ca35215534a4
sunny <sunny@e2serv.com>
2013-10-14 00:59:53 -0700

    first commit

--- js/index.js
+++ js/index.js
@@ -38,7 +38,20 @@ var app = {
     // Update DOM on a Received Event
     receivedEvent: function(id) {
 		alert("dfgdfgdf1")
-		getEmployeeList();
+		var serviceURL = "http://vishalb.in/directory/services/";
+		var employees;
+		$.getJSON(serviceURL + 'getemployees.php', function(data) {
+			$('#employeeList li').remove();
+			employees = data.items;
+			$.each(employees, function(index, employee) {
+				$('#employeeList').append('<li><a href="employeedetails.html?id=' + employee.id + '">' +
+						'<img src="http://vishalb.in/directory/pics/' + employee.picture + '"/>' +
+						'<h4>' + employee.firstName + ' ' + employee.lastName + '</h4>' +
+						'<p>' + employee.title + '</p>' +
+						'<span class="ui-li-count">' + employee.reportCount + '</span></a></li>');
+			});
+			$('#employeeList').listview('refresh');
+		});
 		alert("dfgdfgdfg2")
         console.log('Received Event: ' + id);
     }
